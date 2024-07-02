f3ada44e8a988406d5d0533b24d3fed7b20ba7b5
NikitaJha16 <139324050+NikitaJha16@users.noreply.github.com>
2024-07-02 19:58:10 +0530

    Create NikitaS20.sol

---
+++ NikitaS20.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract NikitaS20 is ERC20 
+{
+    address public owner;
+    mapping(address => bool) private frozenAccounts;
+
+    constructor() ERC20("DISNEY", "DN")
+    {
+        owner = msg.sender;
+        _mint(owner, 7000*10**decimals());
+    }
+
+    modifier onlyOwner() 
+    {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+
+    modifier notFrozen(address account) 
+    {
+        require(!frozenAccounts[account], "Account is frozen");
+        _;
+    }
+
+    function mintTokens(address to, uint256 amount) public onlyOwner 
+    {
+        _mint(to, amount);
+    }
+
+    function burnTokens(uint256 amount) public notFrozen(msg.sender)
+    {
+        _burn(msg.sender, amount);
+    }
+
+    function freezeAccount(address account, bool freeze) public onlyOwner 
+    {
+        frozenAccounts[account] = freeze;
+    }
+
+    function isAccountFrozen(address account) public view returns (bool) 
+    {
+        return frozenAccounts[account];
+    }
+}
